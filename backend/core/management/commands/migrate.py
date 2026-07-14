dab8ba763118596a54433acdc9914482a6bd627a
Ahmed <youremail@gmail.com>
2026-07-14 15:48:36 +0300

    Remove broken migrate.py - fix recursion

--- backend/core/management/commands/migrate.py
+++
@@ -1,7 +0,0 @@
-from django.core.management.base import BaseCommand
-from django.core.management import call_command
-
-class Command(BaseCommand):
-    def handle(self, *args, **kwargs):
-        call_command('migrate')
-        self.stdout.write(self.style.SUCCESS('Migrations applied!'))
