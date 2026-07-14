6d0fa107e009aea8e16c032ec569c357a0505a07
Ahmed <youremail@gmail.com>
2026-07-14 13:01:06 +0300

    Add create_admin command for Render

---
+++ backend/core/management/command/create_admin.py
@@ -0,0 +1,14 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+
+class Command(BaseCommand):
+    def handle(self, *args, **kwargs):
+        if not User.objects.filter(username='admin').exists():
+            User.objects.create_superuser(
+                'admin', 
+                'admin@royalsteel.com', 
+                'admin123456'
+            )
+            self.stdout.write(self.style.SUCCESS('Superuser created!'))
+        else:
+            self.stdout.write(self.style.WARNING('Superuser already exists!'))
